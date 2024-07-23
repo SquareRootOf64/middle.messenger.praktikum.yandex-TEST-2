0c8d7adf36c157d49115a9744c44b2eb8b2d5a34
Olga Lavrukhina <olga-msc1@yandex.ru>
2024-07-23 15:12:04 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite';
 import handlebars from 'vite-plugin-handlebars';
+import resolve from 'path';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                //main: resolve(__dirname, 'index.html'),
+                chats: resolve(__dirname, 'src/chats/chats.html'),
+                error404: resolve(__dirname, 'src/errors/error404.html'),
+                error500: resolve(__dirname, 'src/errors/error500.html'),
+                profile: resolve(__dirname, 'src/profile/profile.html'),
+                register: resolve(__dirname, 'src/register/register.html'),
+            },
+        },
+    },
     plugins: [handlebars()],
 })
